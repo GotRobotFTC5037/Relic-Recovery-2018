53a7f2ad878f309c783a47dee73fd76d1a2fa351
Andrew Garretson <andrew@garretson.org>
2017-11-04 15:37:00 -0500

    Added ColorSensorTest

---
+++ ftc_app-3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/ColorSensorTest.kt
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.ColorSensor
+
+@Autonomous
+class ColorSensorTest: LinearOpMode() {
+    override fun runOpMode() {
+        val colorSensor: ColorSensor = hardwareMap.colorSensor.get("color sensor")
+        colorSensor.enableLed(true)
+        waitForStart()
+
+        while(opModeIsActive()) {
+            telemetry.addData("red", colorSensor.red())
+            telemetry.addData("blue", colorSensor.blue())
+            telemetry.update()
+        }
+    }
+}
